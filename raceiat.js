0dbb1405e4c871db316e0411496fb1f572cfe037
caiogll <166954389+caiogll@users.noreply.github.com>
2024-04-19 12:39:23 +0100

    Update raceiat.js

--- raceiat.js
+++ raceiat.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat8.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://github.com/caiogll/iat/blob/main/sexuality.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 	var global = API.getGlobal();
 
