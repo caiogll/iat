d99ddd02aac0064c18965c7f090af637b520cd0f
Gabriel de Paula <gabriel.lima@flashapp.com.br>
2024-04-19 17:40:19 -0300

    - Adjusts mgr config

--- mgr.js
+++ mgr.js
@@ -8,21 +8,42 @@ define(['managerAPI',
   init_data_pipe(API, 'of0fd6cZlvm1gJD3ilWqg56CoQEqFrcxHMrYtQh4A1UwzkSlApvRDR0TcYBHXlNpJa894E',  {file_type:'csv'});	
 
   API.addTasksSet({
+    intro: [{
+      inherit: 'instructions',
+      name: 'intro',
+      templateUrl: 'intro.jst',
+      title: 'Intro',
+      header: 'Welcome'
+    }],
     sexuality: [{
         type: 'time',
         name: 'sexuality',
         scriptUrl: 'sexuality.js'
+    }],
+    lastpage: [{
+      type: 'message',
+      name: 'lastpage',
+      templateUrl: 'lastpage.jst',
+      title: 'End',
+      //Uncomment the following if you want to end the study here.
+      last:true, 
+      header: 'You have completed the study'
     }], 
 		//This task waits until the data are sent to the server.
     uploading: uploading_task({header: 'just a moment', body:'Please wait, sending data... '})
   });
 
   API.addSequence([
+    {inherit: 'intro'},
     {
-      data :[{
-        inherit:'sexuality'
+      mixer:'random',
+      data:[{
+        mixer: 'wrapper',
+        data:[{ inherit: 'sexualityiat'}]
       }]
-    }
+    },
+		{inherit: 'uploading'},
+    {inherit: 'lastpage'},
   ]);
 
   return API.script;
